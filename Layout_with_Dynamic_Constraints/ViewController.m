3bef185b44e56055ff42e4c1e11e80c7b9e4b196
Tianyi <tianyi.song@rakuten.com>
2020-04-03 09:35:08 +0800

    Initial Commit

---
+++ Layout_with_Dynamic_Constraints/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Layout_with_Dynamic_Constraints
+//
+//  Created by Song, Tianyi | RASIA on 3/4/20.
+//  Copyright © 2020 Song, Tianyi | RASIA. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ Layout_with_Dynamic_ConstraintsTests/Layout_with_Dynamic_ConstraintsTests.m
@@ -0,0 +1,37 @@
+//
+//  Layout_with_Dynamic_ConstraintsTests.m
+//  Layout_with_Dynamic_ConstraintsTests
+//
+//  Created by Song, Tianyi | RASIA on 3/4/20.
+//  Copyright © 2020 Song, Tianyi | RASIA. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Layout_with_Dynamic_ConstraintsTests : XCTestCase
+
+@end
+
+@implementation Layout_with_Dynamic_ConstraintsTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
